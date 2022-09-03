45a711973fbb12c4e2cc390b8109bf61704cf8e1
Luciano Lezana <luchobenjo50@gmail.com>
2022-09-03 18:01:00 -0300

    modificando clase Estudiante

--- app_project_coder/models.py
+++ app_project_coder/models.py
@@ -1,7 +1,7 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
-class soy_una_clase (models.Model):
+class Estudiante (models.Model):
 
     nombre = models.CharField(max_length=130)
     edad = models.IntegerField()
