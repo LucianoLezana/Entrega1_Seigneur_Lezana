704ba7f3ba9867b45ebe3722a1603c28e955bf24
Luciano Lezana <luchobenjo50@gmail.com>
2022-09-03 17:57:44 -0300

    agregando clase estudiante

--- app_project_coder/models.py
+++ app_project_coder/models.py
@@ -1,3 +1,7 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class soy_una_clase (models.Model):
+
+    nombre = models.CharField(max_length=130)
+    edad = models.IntegerField()
