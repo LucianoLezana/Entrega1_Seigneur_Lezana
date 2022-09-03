26c2041e9bdd3c01902365a809e62c04c5c26e73
Luciano Lezana <luchobenjo50@gmail.com>
2022-09-03 18:38:16 -0300

    Modificando archivo .gitignore y agregando clases en la carpeta app_project_coder/models.py

--- app_project_coder/models.py
+++ app_project_coder/models.py
@@ -1,7 +1,29 @@
-from unittest.util import _MAX_LENGTH
+
 from django.db import models
+
+'''Agregando clases:
+Estudiante (nombre, apellido, email)
+Profesor 
+Curso
+'''
+
+
 
 class Estudiante (models.Model):
 
     nombre = models.CharField(max_length=130)
-    edad = models.IntegerField()
+    apellido = models.CharField(max_length=130)
+    email = models.EmailField()
+
+
+class Profesor (models.Model):
+
+    nombre = models.CharField()
+    apellido = models.CharField()
+    email = models.EmailField()
+    profesion = models.CharField()
+
+
+class Curso(models.Model):
+    nombre = models.CharField()
+    comision = models.IntegerField()
