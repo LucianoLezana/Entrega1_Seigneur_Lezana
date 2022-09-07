23d4a41a2b42e3894850bc2a41cef901ddaaa60d
Lucía Roig Seigneur <lucia.roig.seigneur@gmail.com>
2022-09-07 19:11:05 -0300

    agrego estilos a los forms

--- app_project_coder/forms.py
+++ app_project_coder/forms.py
@@ -1,31 +1,54 @@
 from socket import fromshare
 from django import forms
+from .models import Cursos, Estudiantes, Profesores
 
 #Crear formularios para la pagina
 
 #Formulario Cursos:
-class form_curso(forms.Form):
-    nombre = forms.CharField(max_length=120)
-    comision = forms.IntegerField()
+class form_curso(forms.ModelForm):
+    class Meta:
+        model = Cursos
+        fields = ('nombre', 'comision')
+
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control mt-2'}),
+            'comision': forms.TextInput(attrs={'class': 'form-control mt-2'}),
+        }
 
 #-------------------
 
 #Formulario Estudiantes:
-class form_estudiantes(forms.Form): 
-    nombre = forms.CharField(max_length=120)
-    apellido = forms.CharField(max_length=120)
-    email = forms.EmailField()
+
+class form_estudiantes(forms.ModelForm):
+    class Meta:
+        model = Estudiantes
+        fields = ('nombre', 'apellido', 'email')
+
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control mt-2'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control mt-2'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control mt-2'}),
+        }
 
 #-------------------
 
 #Formulario Profesores:
-class form_profesores(forms.Form):
-    nombre = forms.CharField(max_length=120)
-    apellido = forms.CharField(max_length=120)
-    email = forms.EmailField()
-    profesion = forms.CharField(max_length=100)
+
+class form_profesores(forms.ModelForm):
+    class Meta:
+        model = Profesores
+        fields = ('nombre', 'apellido', 'email', 'profesion')
+
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control mt-2'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control mt-2'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control mt-2'}),
+            'profesion': forms.TextInput(attrs={'class': 'form-control mt-2'}),
+        }
 
 #-------------------
+
+
 
 
 
--- app_project_coder/models.py
+++ app_project_coder/models.py
@@ -3,19 +3,20 @@ from django.db import models
 
 '''
         Agregando clases:
+-Curso (nombre, comision)
 -Estudiante (nombre, apellido, email)
 -Profesor (nombre, apellido, email, profesion)
--Curso (nombre, comision)
 
 '''
 
-
+class Cursos(models.Model):
+    nombre = models.CharField(max_length=120)
+    comision = models.IntegerField()
 
 class Estudiantes(models.Model):
     nombre = models.CharField(max_length=120)
     apellido = models.CharField(max_length=120)
     email = models.EmailField()
-
 
 class Profesores(models.Model):
     nombre = models.CharField(max_length=120)
@@ -23,7 +24,3 @@ class Profesores(models.Model):
     email = models.EmailField()
     profesion = models.CharField(max_length=100)
 
-
-class Cursos(models.Model):
-    nombre = models.CharField(max_length=120)
-    comision = models.IntegerField()
